30bef061b27453e993eedd739fb66ac592e2343f
Mahmoud Ayman <mahmoud.ayman.fcai@gmail.com>
2025-05-21 20:00:09 +0300

    Read Products

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var product_list = context.Products.ToList();
+            return View(product_list);
+        }
+    }
+}
